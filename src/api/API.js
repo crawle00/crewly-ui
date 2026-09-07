5e065fb330ce35a9869431ea35c11f74f752ee93
Ryan Cha <rcha00@uafs.edu>
2026-09-06 22:25:56 -0500

    Finished user side ui

--- src/api/API.js
+++ src/api/API.js
@@ -62,6 +62,14 @@ export async function getClubs(params) {
   return response.data;
 }
 
+export async function getClub(id) {
+  const response = await axios.get(
+    `${API_URL}/clubs/${id}`,
+    requestConfig
+  )
+  return response.data
+}
+
 export async function deleteClub(id) {
   const response = await axios.delete(`${API_URL}/clubs/${id}`, requestConfig);
   return response.data;
@@ -122,17 +130,29 @@ export async function getFaq(listingId) {
   return response.data
 }
 
-export async function createFaq(listingId , question) {
-  const response = await axios.post(`${API_URL}/faq`, listingId , question , requestConfig)
+
+export async function createFaq(listingId, question) {
+  const response = await axios.post(`${API_URL}/faq`, {listingId , question,},requestConfig
+  )
+
   return response.data
 }
 
 export async function createReports(listingId , reports) {
-  const response = await axios.post(`${API_URL}/faq`, listingId , reports , requestConfig)
+  const response = await axios.post(`${API_URL}/reports`, {listingId , reports} , requestConfig)
   return response.data
 }
 
 export async function getVolunteers(listingId) {
-  const response = await axios.get(`${API_URL}/volunteers/${listingId}` , requestConfig)
+  const response = await axios.get(`${API_URL}/jobs/listing/${listingId}/volunteers` , requestConfig)
+  return response.data
+}
+
+export async function volunteerForListing(listingId) {
+  const response = await axios.post(
+    `${API_URL}/jobs/listing/${listingId}/volunteers`,
+    null,
+    requestConfig
+  )
   return response.data
 }
